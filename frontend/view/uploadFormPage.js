193cd45c0681eecca05317832a77d6f2fda0b94e
jabulani171 <87236039+jabulani171@users.noreply.github.com>
2025-06-20 14:14:05 +0200

    Update uploadFormPage.js

--- frontend/view/uploadFormPage.js
+++ frontend/view/uploadFormPage.js
@@ -2,6 +2,13 @@ import {useState} from 'react';
 import {useRouter} from 'next/router';
 
 export default function Home() {
-  let firstName = useState('')
+  let firstName = useState('');
+  let lastname = useState('');
+  let dateOfBirth = useState('');
+  let file = useState(null);
+  let error = useState('');
+  let router = useRouter();
+
+  const
 }
 
