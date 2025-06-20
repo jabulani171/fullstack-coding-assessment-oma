90221b513174b199e63e50cbfcf7310bc475bd39
jabulani171 <87236039+jabulani171@users.noreply.github.com>
2025-06-20 15:46:08 +0200

    Create main.js

---
+++ Backend/main.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const multer = require('multer');
+const pdfParse = require('pdf-parser');
+const Tesseract = require('tesseract.js');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+
+
+
+    
