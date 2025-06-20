8618d5df86e8e7726877c64d80b83aef094d6b47
jabulani171 <87236039+jabulani171@users.noreply.github.com>
2025-06-20 14:51:24 +0200

    Update uploadFormPage.js

--- frontend/view/uploadFormPage.js
+++ frontend/view/uploadFormPage.js
@@ -56,7 +56,7 @@ export default function Home() {
 
   return ( 
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <h1>Upload Form</h1>
+        <h1>User Input Details and File upload</h1>
     
     )
   
