478ef918e3090b95060c74325cfd9feae78d7e23
jabulani171 <87236039+jabulani171@users.noreply.github.com>
2025-06-20 15:36:18 +0200

    Update resultDisplayPage.js

--- frontend/view/resultDisplayPage.js
+++ frontend/view/resultDisplayPage.js
@@ -40,4 +40,7 @@ if(!output)
     </div>
 
   <button onClick={() => router.push('/') } className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600">Return to home page</button>
-  )
+  </div>
+  </div>
+);
+}
